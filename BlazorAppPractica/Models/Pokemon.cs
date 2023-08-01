9797415bec551a41b489126a2199a54788ae2e44
Heriberto Robles Coronado <rrobleshc@hotmail.com>
2023-07-31 17:15:52 -0700

    Add project files.

---
+++ BlazorAppPractica/Models/Pokemon.cs
@@ -0,0 +1,18 @@
+﻿namespace BlazorAppPractica.Models
+{
+    public class Pokemon
+    {
+        public string name { get; set; }
+        public string url { get; set; }
+    }
+
+    
+
+    public class Root
+    {
+        public int count { get; set; }
+        public string next { get; set; }
+        public object previous { get; set; }
+        public List<Pokemon> results { get; set; }
+    }
+}
---
+++ BlazorAppPractica/Program.cs
@@ -0,0 +1,12 @@
+using BlazorAppPractica;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://pokeapi.co/api/v2/pokemon/") });
+
+await builder.Build().RunAsync();
+
